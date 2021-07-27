df79ec620f61240f4e02261a628a3d2103ea2bcf
Erico Alexandre <ericoabs25@gmail.com>
2021-07-27 18:54:58 -0300

    Configuração de eslint

--- App.tsx
+++ App.tsx
@@ -1,35 +1,13 @@
 import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
   Text,
 } from 'react-native';
 
-const App = () => {
+export const App = () => {
   return (
     <SafeAreaView>
       <Text>Olá</Text>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
